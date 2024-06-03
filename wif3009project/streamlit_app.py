7b64dbe251b7f78507e36cbe72345f8e176732ff
SamerAbukhader <sakhader_2003@hotmail.com>
2024-06-03 14:25:29 +0800

    More Fixes

--- wif3009project/streamlit_app.py
+++ wif3009project/streamlit_app.py
@@ -1,60 +1,11 @@
-from pywebio.input import input, input_group, FLOAT, select, checkbox
-from pywebio.output import put_html
-from pywebio import start_server
+import streamlit as st
 import numpy as np
-
 import joblib
 
+# Load the model
 model = joblib.load('HousePricing.pkl')
 
-html = """
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Predicted House Price</title>
-    <style>
-      body {
-        font-family: Arial, sans-serif;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        margin: 0;
-      }
-      h1 {
-        color: #333;
-        font-size: 3rem;
-      }
-      p {
-        color: #333;
-        font-size: 2rem;
-      }
-      a {
-        color: #126bd7;
-        text-decoration: none;
-        font-size: 1.5rem;
-        margin-top: 20px;
-      }
-      #predictedPrice {
-        color: green;
-        font-size: 2.5rem;
-      }
-    </style>
-  </head>
-  <body>
-    <h1>Predicted House Price</h1>
-    <p>
-      The predicted house price is: <span id="predictedPrice">RM %price%</span>
-    </p>
-    <a href="">Go back</a>
-  </body>
-</html>
-"""
-
-
+# Mapping dictionaries
 property_type_dict = {
     "1-sty Terrace/Link House": 0,
     "1.5-sty Terrace/Link House": 1,
@@ -143,38 +94,30 @@ furnishing_mapping = {
     'Unfurnished': (0, 0, 1)
 }
 
-property_type_keys = list(property_type_dict.keys())
-location_keys = list(location_dict.keys())
+st.set_page_config(
+    page_title="House Price Prediction",
+    page_icon="🏠",
+)
 
-full_furnishing = 0
-part_furnishing = 0
-unfurnishing = 0
+# Streamlit app
+st.title('House Price Prediction')
 
+bedrooms = st.number_input('Enter the number of bedrooms', min_value=0.0, format='%f')
+bathrooms = st.number_input('Enter the number of bathrooms', min_value=0.0, format='%f')
+sqft = st.number_input('Enter the House Size in SqFT', min_value=0.0, format='%f')
+furnishing = st.selectbox('Select Furnishing', list(furnishing_mapping.keys()))
+price_per_sqft = st.number_input('Enter the Price per Sqft', min_value=0.0, format='%f')
+property_type = st.selectbox('Select Property Type', list(property_type_dict.keys()))
+car_parks = st.number_input('Enter the number of car parks', min_value=0.0, format='%f')
+location = st.selectbox('Select Location', list(location_dict.keys()))
 
-def predictPrice():
-    form = input_group("House Price Prediction",
-            [
-            input("Enter the number of bedrooms", name="bedrooms", type=FLOAT, required=True),
-            input("Enter the number of bathrooms", name="bathrooms", type=FLOAT, required=True),
-            input("Enter the number of House Size in SqFT", name="sqft", type=FLOAT, required=True),
-            checkbox("Select Furnishing", options=['Fully Furnished', 'Partly Furnished', 'Unfurnished'], name="furnishing", inline=True, required=True),
-            input("Enter the Price per Sqft", name="price_per_sqft", type=FLOAT, required=True),
-            select("Select Property Type", options=property_type_keys, name="property_type"),
-            input("Enter the number of car parks", name="car_parks", type=FLOAT, required=True),
-            select("Select Location", options=location_keys, name="location")
-            ])
-    bedrooms = form['bedrooms']
-    bathrooms = form['bathrooms']
-    sqft = form['sqft']
-    price_per_sqft = form['price_per_sqft']
-    furnishing = form['furnishing'][0]
-    car_parks = form['car_parks']
-    property_type = property_type_dict[form['property_type']]
-    location = location_dict[form['location']]
-    full_furnishing, part_furnishing, unfurnishing = furnishing_mapping.get(furnishing, (0, 0, 1))
-    X = np.array([[location, bedrooms, bathrooms, car_parks, property_type, sqft, full_furnishing, part_furnishing, unfurnishing, price_per_sqft]])
+if st.button('Predict Price'):
+    property_type_value = property_type_dict[property_type]
+    location_value = location_dict[location]
+    full_furnishing, part_furnishing, unfurnishing = furnishing_mapping[furnishing]
+
+    X = np.array([[location_value, bedrooms, bathrooms, car_parks, property_type_value, sqft, full_furnishing, part_furnishing, unfurnishing, price_per_sqft]])
     price = model.predict(X)
     predicted_price = "{:,.2f}".format(price[0])
-    put_html(html.replace("%price%", predicted_price))
-    
-start_server(predictPrice, port=8080)
+
+    st.write(f"The predicted house price is: RM {predicted_price}")
